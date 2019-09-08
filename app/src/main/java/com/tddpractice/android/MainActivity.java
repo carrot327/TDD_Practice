9f8a6320aab34b0bd44ed0f5b522965d1c400c9c
carrot327 <1208484060@qq.com>
2019-09-08 18:20:22 +0800

    已导入assertJ

--- app/src/main/java/com/tddpractice/android/MainActivity.java
+++ app/src/main/java/com/tddpractice/android/MainActivity.java
@@ -10,23 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for (int i = 0; i <= 100; i++) {
-            String result = checkFizzBuzz(i);
-            Log.d("MainActivity", i + ":" + result);
-        }
-    }
-
-
-    public static String checkFizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        }
-        if (number % 3 == 0) {
-            return "Fizz";
-        }
-        if (number % 5 == 0) {
-            return "Buzz";
-        }
-        return null;
     }
 }
